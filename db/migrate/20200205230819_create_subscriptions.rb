61b51b184abd94f7689ccd0c590ae8dc27a1936e
LFM Alves <luis.projensino@gmail.com>
2020-02-06 10:39:32 -0300

    fix: schema corrections, book way not working

--- db/migrate/20200205230819_create_subscriptions.rb
+++ db/migrate/20200205230819_create_subscriptions.rb
@@ -1,10 +1,12 @@
 class CreateSubscriptions < ActiveRecord::Migration[6.0]
   def change
     create_table :subscriptions do |t|
-      t.references :leader, null: false, foreign_key: true
-      t.references :follower, null: false, foreign_key: true
+      t.references :leader, null: false, foreign_key: false
+      t.references :follower, null: false, foreign_key: false
 
       t.timestamps
     end
+    add_foreign_key :subscriptions, :users, column: :leader_id # This is a quick fix, the book way isnt working
+    add_foreign_key :subscriptions, :users, column: :follower_id # This is a quick fix, the book way isnt working
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,6 @@ ActiveRecord::Schema.define(version: 2020_02_05_230819) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "subscriptions", "followers"
-  add_foreign_key "subscriptions", "leaders"
+  add_foreign_key "subscriptions", "users", column: "follower_id"
+  add_foreign_key "subscriptions", "users", column: "leader_id"
 end
