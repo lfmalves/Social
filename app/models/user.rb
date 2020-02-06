08c32389996bc0332597281b88203b00570a6319
LFM Alves <luis.projensino@gmail.com>
2020-02-06 17:06:59 -0300

    feat: has_many assocs for USer

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
   has_many :leaders, through: :subscriptions
   has_many :reverse_subscriptions, foreign_key: :leader_id, class_name: 'Subscription', dependent: :destroy
   has_many :followers, through: :reverse_subscriptions
+  has_many :posts, dependent: :destroy
+  has_many :text_posts, dependent: :destroy
+  has_many :image_posts, dependent: :destroy
 
   def following?(leader)
     leaders.include? leader
