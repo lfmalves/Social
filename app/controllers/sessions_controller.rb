5382fa4d54826d2a43f3d8281fd73c631b725d6a
LFM Alves <luis.projensino@gmail.com>
2020-02-11 12:21:44 -0300

    feat: new, create and destroy methods for the Sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 # frozen_string_literal: true
 
 class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url, notice: 'Log in successful!'
+    else
+      flash.now.alert = 'Invalid email or password'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: 'Log out successful!'
+  end
 end
