9c2fb3015270d4650ad5c2fb826a7fbef0848dee
LFM Alves <luis.projensino@gmail.com>
2020-02-11 13:57:15 -0300

    feat: log out, log in and sign up links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,10 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-
   private
 
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find(session[:user_id])
-    end
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   helper_method :current_user
