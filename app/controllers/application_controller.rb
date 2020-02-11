62cce02509f06b4fcbda48523d9db4bd8b168448
LFM Alves <luis.projensino@gmail.com>
2020-02-11 13:48:07 -0300

    feat: helper_method current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+
+  private
+
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    end
+  end
+
+  helper_method :current_user
 end
